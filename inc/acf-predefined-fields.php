85e3a4a3857ae90988b918e397bae5333c55267d
Jonathan Biddle <fenix429@gmail.com>
2015-05-06 10:10:38 -0400

    Updated ACF Predefined Fields

--- inc/acf-predefined-fields.php
+++ inc/acf-predefined-fields.php
@@ -4,60 +4,109 @@
  * 
  */
 
-if(function_exists("register_field_group"))
-{
-	register_field_group(array (
-		'id' => 'acf_page-options',
-		'title' => 'Page Options',
-		'fields' => array (
-			array (
-				'key' => 'field_54206c5db6a34',
-				'label' => 'Override Page Title?',
-				'name' => 'hide_title',
-				'type' => 'true_false',
-				'message' => '',
-				'default_value' => 0,
+if( function_exists('register_field_group') ):
+
+register_field_group(array (
+	'key' => 'group_55439a7e8a79d',
+	'title' => 'Layout Options',
+	'fields' => array (
+		array (
+			'key' => 'field_55439ac8e4556',
+			'label' => 'Remove Entry Header Block',
+			'name' => 'remove_entry_header',
+			'prefix' => '',
+			'type' => 'true_false',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array (
+				'width' => 50,
+				'class' => 'tf-block',
+				'id' => '',
 			),
-			array (
-				'key' => 'field_54206c7ab6a35',
-				'label' => 'Entry Header Content',
-				'name' => 'custom_header',
-				'type' => 'textarea',
-				'conditional_logic' => array (
-					'status' => 1,
-					'rules' => array (
-						array (
-							'field' => 'field_54206c5db6a34',
-							'operator' => '==',
-							'value' => '1',
-						),
-					),
-					'allorany' => 'all',
-				),
-				'default_value' => '',
-				'placeholder' => '',
-				'maxlength' => '',
-				'rows' => '',
-				'formatting' => 'html',
-			),
+			'message' => '',
+			'default_value' => 0,
 		),
-		'location' => array (
-			array (
+		array (
+			'key' => 'field_5543a04ae4557',
+			'label' => 'Override Page Title?',
+			'name' => 'hide_title',
+			'prefix' => '',
+			'type' => 'true_false',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => array (
 				array (
-					'param' => 'post_type',
-					'operator' => '==',
-					'value' => 'page',
-					'order_no' => 0,
-					'group_no' => 0,
+					array (
+						'field' => 'field_55439ac8e4556',
+						'operator' => '!=',
+						'value' => '1',
+					),
 				),
 			),
+			'wrapper' => array (
+				'width' => 50,
+				'class' => 'tf-block',
+				'id' => '',
+			),
+			'message' => '',
+			'default_value' => 0,
 		),
-		'options' => array (
-			'position' => 'acf_after_title',
-			'layout' => 'no_box',
-			'hide_on_screen' => array (
+		array (
+			'key' => 'field_5543a0fce4558',
+			'label' => 'Entry Header Content',
+			'name' => 'custom_header',
+			'prefix' => '',
+			'type' => 'wysiwyg',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => array (
+				array (
+					array (
+						'field' => 'field_5543a04ae4557',
+						'operator' => '==',
+						'value' => '1',
+					),
+					array (
+						'field' => 'field_55439ac8e4556',
+						'operator' => '!=',
+						'value' => '1',
+					),
+				),
+			),
+			'wrapper' => array (
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'tabs' => 'all',
+			'toolbar' => 'full',
+			'media_upload' => 1,
+		),
+	),
+	'location' => array (
+		array (
+			array (
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'post',
 			),
 		),
-		'menu_order' => 0,
-	));
-}
+		array (
+			array (
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'page',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'acf_after_title',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+));
+
+endif;
